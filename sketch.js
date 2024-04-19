3020e1a15f9f63242b4d26fd9b6d009e8d9d8902
Jade570 <chaeryeongoh@gatech.edu>
2024-04-19 13:10:34 -0400

    Add water ball

--- sketch.js
+++ sketch.js
@@ -3,16 +3,30 @@ let cam1 = new p5((sketch) => {
   let flower;
   let flowers;
   let cells;
+  let water;
+  let theShader;
+  let shaderTexture;
+
   let flowerNum = 0;
   let patternNum = 1;
-  let shaderNum = 0;
+  let shaderNum = 3;
   let fade = 0;
+
+
+  let theta = 0;
+
+let x;
+let y;
+let outsideRadius = 200;
+let insideRadius = 100;
 
   sketch.preload = () => {
     // load the shader
     flower = sketch.loadShader("shader.vert", "shaders/flower.frag");
     flowers = sketch.loadShader("shader.vert", "shaders/flowers.frag");
     cells = sketch.loadShader("shader.vert", "shaders/cells.frag");
+    water = sketch.loadShader("shader.vert", "shaders/water.frag");
+    theShader = sketch.loadShader("shaders/shape.vert", "shaders/water.frag");
   };
 
   sketch.setup = () => {
@@ -26,9 +40,17 @@ let cam1 = new p5((sketch) => {
       document.getElementById("cam1")
     );
     sketch.noStroke();
+
+
+    shaderTexture = sketch.createGraphics(710, 400, sketch.WEBGL);
+    shaderTexture.noStroke();
+    x = -50;
+    y = 0;
   };
 
   sketch.draw = () => {
+
+
     // send resolution of sketch into shader
     flower.setUniform("u_time", sketch.frameCount * 0.01);
     flower.setUniform("u_resolution", [sketch.width, sketch.windowHeight]);
@@ -52,6 +74,16 @@ let cam1 = new p5((sketch) => {
     flowers.setUniform("u_flowerNum", flowerNum);
     flowers.setUniform("u_patternNum", patternNum);
     flowers.setUniform("u_fade", fade);
+
+    // send resolution of sketch into shader
+    water.setUniform("u_time", sketch.frameCount * 0.01);
+    water.setUniform("u_resolution", [sketch.width, sketch.windowHeight]);
+    water.setUniform("u_mouse", [sketch.mouseX, sketch.mouseY]);
+
+
+    theShader.setUniform("u_time", sketch.frameCount * 0.01);
+    theShader.setUniform("u_resolution", [sketch.width, sketch.windowHeight]);
+    theShader.setUniform("u_mouse", [sketch.mouseX, sketch.mouseY]);
     // shader() //sets the active shader with our shader
 
     switch (shaderNum) {
@@ -64,14 +96,31 @@ let cam1 = new p5((sketch) => {
       case 2:
         sketch.shader(flowers);
         break;
+      case 3:
+        sketch.shader(water);
+        break;
     }
     // sketch.shader(flower);
-
+    shaderTexture.shader(theShader);
     // rect gives us some geometry on the screen
-    sketch.rect(0, 0, sketch.windowWidth, sketch.height);
+    shaderTexture.rect(0, 0, sketch.width, sketch.height);
     // console.log(sketch.frameCount*0.01);
     //  console.log(shaderNum);
     //sketch.text("hi")
+
+    shaderTexture.rect(0,0,sketch.width, sketch.height);
+    sketch.background(244);
+    sketch.texture(shaderTexture);
+  
+    sketch.translate(-150, 0, 0);
+    sketch.push();
+    sketch.rotateZ(theta * sketch.mouseX * 0.0001);
+    sketch.rotateX(theta * sketch.mouseX * 0.0001);
+    sketch.rotateY(theta * sketch.mouseX * 0.0001);  
+    theta += 0.05;
+    sketch.sphere(125);
+    sketch.pop();
+    
   };
 
   sketch.keyPressed = () => {
@@ -84,20 +133,22 @@ let cam1 = new p5((sketch) => {
       patternNum--;
       fade -= 0.1;
     }
-    
+
     switch (sketch.key) {
-      
-      case '0':
+      case "0":
         console.log(sketch.key);
         shaderNum = 0;
         break;
-      case '1':
+      case "1":
         shaderNum = 1;
         break;
 
-      case '2':
+      case "2":
         shaderNum = 2;
         break;
+        case "3":
+          shaderNum = 3;
+          break;
     }
   };
 
